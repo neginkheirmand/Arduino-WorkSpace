5be5c9b7fdf031a8ab1771cbd8d4bfb038c7179f
neginkheirmand <neginkheirmand@gmail.com>
2020-10-24 13:40:16 +0330

    added first code the verify file

---
+++ MainCode/Verify/Verify.ino
@@ -0,0 +1,20 @@
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  Serial.begin(9600);
+  Serial.println("start Setup()");
+
+  Serial.println("start Setup()");
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+    Serial.println("start Loop()");
+
+    Serial.println("end Loop()");
+
+  
+}
