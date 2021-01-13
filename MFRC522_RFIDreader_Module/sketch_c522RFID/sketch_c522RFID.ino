559b06d28ae39c05c2e9f3ba8140198e90e2e93a
neginkheirmand <neginkheirmand@gmail.com>
2021-01-13 22:00:03 +0330

    added code for reading UID of any card with the working-frequency  of 13.56 Mhz

--- MFRC522_RFIDreader_Module/sketch_c522RFID/sketch_c522RFID.ino
+++ MFRC522_RFIDreader_Module/sketch_c522RFID/sketch_c522RFID.ino
@@ -1,9 +1,42 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+  MFRC522       Arduino Nano      Arduino Uno
+  3.3 v         3.3v              3.3v
+  GND           GND               GND 
+  IRQ           -                 -           // this pin won't be used
+  RST           9                 D9
+  SPI SS        10                D10        
+  SPI MOSI      11                D11        
+  SPI MISO      12                D12        
+  SPI SCK       13                D13        
+*/
 
+
+
+#include <SPI.h>      //include SPI library
+#include <MFRC522.h>      // include the MFRC522, link in the info.txt file
+
+#define RST_PIN  9       
+#define SS_PIN  10      
+
+MFRC522 mfrc522(SS_PIN, RST_PIN); //init mfrc522 objet 
+
+void setup() {
+  Serial.begin(9600);     // initialize the comunicacion for monitor serie at 9600 bps
+  SPI.begin();        // initialize SPI bus 
+  mfrc522.PCD_Init();     // initialize the Module
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if ( ! mfrc522.PICC_IsNewCardPresent()) // if no card is detected
+    return;         
+  
+  if ( ! mfrc522.PICC_ReadCardSerial())   // if cannot detect any data from the card
+    return;         
+    
+  Serial.print("UID:");       // print "UID: " text in the serial monitor
+  for (byte i = 0; i < mfrc522.uid.size; i++) { // loop through each byte of the UID
+    Serial.print(mfrc522.uid.uidByte[i], HEX);  // print the byte in hex-mode
+  } 
+  Serial.println();       
+  mfrc522.PICC_HaltA();     //end communication with the card              
 }
