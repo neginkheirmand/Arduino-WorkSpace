dab22aee94e98f3a8fee007eef0aaf95ef786117
neginkheirmand <neginkheirmand@gmail.com>
2021-01-13 17:14:52 +0330

    created MFRC522_RFIDreader_Module folder

--- NRF24L_Module/code/code.ino
+++ NRF24L_Module/code/code.ino
@@ -5,6 +5,18 @@
 #include <RF24.h>
 
 
-RF24 radio(7,8);
-void setup(){}
-void loop(){}
+RF24 radio(7,8); // CNS, CE
+const byte address[6] = "00010";
+void setup(){
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+  Serial.begin(115200);
+}
+void loop(){
+  const char message[]= "Hello World!";
+  radio.write(&message, sizeof(message));
+  delay(1000);
+  Serial.println("done");
+}
